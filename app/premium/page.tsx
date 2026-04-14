651baf29e311d954254da3e2467bd0595e1d9d28
Manu10 <91377863+Manu10Apps@users.noreply.github.com>
2026-04-14 14:30:51 +0200

    Files Upload

--- app/premium/page.tsx
+++ app/premium/page.tsx
@@ -123,7 +123,7 @@ export default function PremiumPage() {
             className="h-16 w-16 rounded-lg sm:h-20 sm:w-20"
             priority
           />
-          <p className="text-center mt-3 text-4xl font-normal font-sinbad-the-sailor text-neutral-900 dark:text-white">
+          <p className="text-center mt-3 text-2xl font-normal font-sinbad-the-sailor text-neutral-900 dark:text-white">
             Intambwe Media
           </p>
           {/* Language Switcher */}
