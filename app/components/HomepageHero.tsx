7fe4df65630cf1037c89d59e86f7c299f1a02a8a
Manu10 <91377863+Manu10Apps@users.noreply.github.com>
2026-04-20 11:13:55 +0200

    files Upload

--- app/components/HomepageHero.tsx
+++ app/components/HomepageHero.tsx
@@ -92,15 +92,11 @@ export function HomepageHero({
     <section className="py-6 bg-white dark:bg-neutral-900 border-b border-neutral-200 dark:border-neutral-800">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="relative grid grid-cols-1 gap-6 pb-10 md:grid-cols-4">
-          <div className="absolute bottom-0 left-1/2 z-10 -translate-x-1/2 rotate-180 lg:left-auto lg:right-0 lg:translate-x-0">
-            <div className="imv-header-nav shrink-0">
-              <div className="imv-header-nav-title">
-                <span className="-rotate-180 inline-block">{t.home.featured}</span>
-                <span className="a1" />
-                <span className="a2" />
-                <span className="a3" />
-                <span className="a4" />
-              </div>
+          <div className="absolute bottom-0 left-1/2 z-10 -translate-x-1/2 lg:left-auto lg:right-0 lg:translate-x-0">
+            <div className="modern-header-section shrink-0">
+              <h2 className="modern-header-title">
+                {t.home.featured}
+              </h2>
             </div>
           </div>
 
