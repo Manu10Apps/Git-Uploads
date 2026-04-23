532964b1846d95f5c3ea5fe3539efdbcf8541408
Manu10 <91377863+Manu10Apps@users.noreply.github.com>
2026-04-24 02:45:33 +0300

    files upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { Header, Footer, BreakingNewsCarousel } from './components';
 import { DeferredHomePageFeed } from '@/app/components/DeferredHomePageFeed';
-import SportsArticlesRow from '@/app/components/SportsArticlesRow';
 import { HomepageHero } from '@/app/components/HomepageHero';
 import { NewsletterSignup } from '@/app/components/NewsletterSignup';
 import { getFeaturedHomepageData, getHomepageData } from '@/lib/homepage-data';
@@ -35,8 +34,6 @@ export default async function Home() {
         )}
 
         <HomepageAdverts topAdverts={homepageTopAdverts} bottomAdverts={homepageBottomAdverts} />
-
-        <SportsArticlesRow />
 
         {/* Deferred loading for full feed */}
         <DeferredHomePageFeed />
